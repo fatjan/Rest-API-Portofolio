6def08253146f0deb55137a081aa534a79d5903a
fatmajanna <fatma.janna.utp@gmail.com>
2019-03-20 17:24:56 +0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ cd /home/ubuntu/Rest-API-Portofolio &&
 git pull
 
 source ~/.profile
-echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
+echo "$DOCKERHUB_PASS" | sudo docker login --username $DOCKERHUB_USER --password-stdin
 sudo docker stop ecommerce_container1007
 sudo docker rm ecommerce_container1007
 sudo docker rmi fatjan/ecommerce
