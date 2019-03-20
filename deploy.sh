dbe6dea2a5589e0564ed21ac0475413d09b67984
fatmajanna <fatma.janna.utp@gmail.com>
2019-03-20 10:47:15 +0700

    upload file deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+eval "$(ssh-agent -s)" &&
+ssh-add -k ~/.ssh/id_rsa &&
+cd /home/ubuntu/Rest-API-Portofolio
+sudo git pull
+
+sudo source ~/.profile
+sudo echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
+sudo docker stope ecommerce_container1007
+sudo docker rm ecommerce_container1007
+sudo docker rmi fatjan/ecommerce
+sudo docker run -d --name ecommerce_container1007 -p 5001:5001 fatjan/ecommerce:production
