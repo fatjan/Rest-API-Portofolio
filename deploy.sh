697df79da5db0354287739b6bb51d0e8a08b10b8
fatmajanna <fatma.janna.utp@gmail.com>
2019-03-20 19:51:44 +0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ git pull
 
 source ~/.profile
 echo "$DOCKERHUB_PASS" | sudo docker login --username $DOCKERHUB_USER --password-stdin
-sudo docker stop ecommerce_container1007
-sudo docker rm ecommerce_container1007
-sudo docker rmi fatjan/ecommerce
+sudo docker stop $(docker ps -a -q)
+sudo docker rm $(docker ps -a -q)
+sudo docker rmi $(sudo docker images -q)
 sudo docker run -d --name ecommerce_container1007 -p 5001:5001 fatjan/ecommerce:latest
